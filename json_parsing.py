1a23a9a01dcafe6835affa7c84d90c518349f74e
GinaShev <ginashev@gmail.com>
2021-10-15 15:31:28 +0200

    json pars

---
+++ json_parsing.py
@@ -0,0 +1,12 @@
+import json
+
+string_as_json_format = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(string_as_json_format)
+key = "messages"
+
+if key in obj:
+    print(obj[key][1])
+else:
+    print(f"No key {key} in JSON")
+
+
