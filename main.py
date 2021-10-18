2c4022af9971378d9dd4f5b3c8390f8c7c0a5e33
GinaShev <ginashev@gmail.com>
2021-10-18 15:15:33 +0200

    response for different methods

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from json.decoder import JSONDecodeError
 import requests
 
+response = ""
 
-response = requests.get(" https://playground.learnqa.ru/api/long_redirect")
-print(len(response.history)-1)
-print(response.url)
+methods = ["POST", "GET", "PUT", "DELETE", "HEAD"
+           ]
+for met in methods:
+    if met == "POST":
+        response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
+        print(response.url)
+        print(response.text)
+    if met == "GET":
+        response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
+        print(response.url)
+        print(response.text)
+    if met == "PUT":
+        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
+        print(response.url)
+        print(response.text)
+    if met == "DELETE":
+        response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
+        print(response.url)
+        print(response.text)
+    else:
+        response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
+        print(response.url)
+        print(response.text)
