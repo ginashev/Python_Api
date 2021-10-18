6ad61400aeea4656a0828da283684316bb9c321b
GinaShev <ginashev@gmail.com>
2021-10-18 15:30:37 +0200

    response for different methods

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 response = ""
 
-methods = ["GET", "POST", "PUT", "DELETE", "HEAD"]
+methods = ["GET", "POST", "PUT", "DELETE"]
 for met in methods:
     if met == "GET":
         response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
@@ -17,9 +17,7 @@ for met in methods:
     if met == "DELETE":
         response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": met})
         print(met + response.text)
-    if met == "HEAD":
-        response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type")
-        print(met + response.text)
+
 
 methods = ["GET", "POST", "PUT", "DELETE"]
 for met in methods:
