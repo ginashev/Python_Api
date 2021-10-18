5fff79d736c3284a387bf4845e61b33f02a2de31
GinaShev <ginashev@gmail.com>
2021-10-18 15:23:21 +0200

    response for different methods

--- main.py
+++ main.py
@@ -3,26 +3,20 @@ import requests
 
 response = ""
 
-methods = ["POST", "GET", "PUT", "DELETE", "HEAD"
-           ]
+methods = ["GET", "POST", "PUT", "DELETE", "HEAD"]
 for met in methods:
-    if met == "POST":
-        response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
-        print(response.url)
-        print(response.text)
     if met == "GET":
         response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
-        print(response.url)
-        print(response.text)
+        print(met + response.text)
+    if met == "POST":
+        response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": met})
+        print(met + response.text)
     if met == "PUT":
-        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
-        print(response.url)
-        print(response.text)
+        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": met})
+        print(met + response.text)
     if met == "DELETE":
-        response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
-        print(response.url)
-        print(response.text)
-    else:
-        response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": met})
-        print(response.url)
-        print(response.text)
+        response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": met})
+        print(met + response.text)
+    if met == "HEAD":
+        response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type")
+        print(met + response.text)
